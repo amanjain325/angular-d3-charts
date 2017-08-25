8f8f485b72e6f30b4fa1512201bc378274bee711
amanjain325 <aman.jain@algoworks.com>
2017-08-25 12:48:52 +0530

    added icons

--- src/app/doughnut-chart/doughnut-chart.component.ts
+++ src/app/doughnut-chart/doughnut-chart.component.ts
@@ -10,19 +10,12 @@ declare let $: any;
 })
 export class DoughnutChartComponent implements OnInit {
   public selectedId: any;
-  public images: Object = {
-    2: 'assets/img/flush_final.png',
-    4: 'assets/img/Other_final.png',
-    8: 'assets/img/faucet_final.png',
-    10: 'assets/img/shower_final.png',
-    20: 'assets/img/Irrigation_final.png',
-  };
   public slicesImages: Object = {
-    2: 'assets/img/flushSlice.png',
-    4: 'assets/img/others_slice.png',
-    8: 'assets/img/faucet_slice.png',
-    10: 'assets/img/showerSlice.png',
-    20: 'assets/img/irrigationSlice.png',
+    2: 'assets/img/1.png',
+    4: 'assets/img/2.png',
+    8: 'assets/img/3.png',
+    10: 'assets/img/4.png',
+    20: 'assets/img/5.png',
   };
   public colorSelect: Object = {
     2: '#ff8000',
@@ -49,36 +42,31 @@ export class DoughnutChartComponent implements OnInit {
       id: 0,
       label: 'slice 1',
       percentage: 10,
-      value: 10,
-
+      value: 10
     }, {
       event: 4,
       id: 1,
       label: 'slice 2',
       percentage: 20,
       value: 20,
-
     }, {
       event: 8,
       id: 2,
       label: 'slice 3',
       percentage: 30,
       value: 30,
-
     }, {
       event: 10,
       id: 3,
       label: 'slice 4',
       percentage: 20,
       value: 20,
-
     }, {
       event: 20,
       id: 4,
       label: 'slice 5',
       percentage: 20,
       value: 10,
-
     }];
 
     let pie = d3.layout.pie()
@@ -146,14 +134,6 @@ export class DoughnutChartComponent implements OnInit {
             }
           });
       });
-    //     .transition().delay(function (d, i) { return i * 200; }).duration(100)
-    // .attrTween('d', function (d) {
-    //     let i = d3.interpolate(d.startAngle + 0.1, d.endAngle);
-    //     return function (t) {
-    //         d.endAngle = i(t);
-    //         return arc(d);
-    //     }
-    // });
 
     g.append('g')
       .attr('transform', function (d) {
@@ -211,24 +191,21 @@ export class DoughnutChartComponent implements OnInit {
       .attr('y', -60)
       .attr('width', 120)
       .attr('height', 120)
-      .attr('cursor', 'pointer');
+      .attr('cursor', 'pointer').attr('xlink:href', 'assets/img/6.png')
+      .on('click', function click(d) {
+        alert('1');
+      });
 
     g.on('click', function click(d) {
       if (d.data.expanded) {
         d3.select('#centerData').style('display', 'block');
-        $('#slice_image').attr('src', d.data.image);
-        $('#slice_value').text(d.data.label + '\n' +
-          d.data.value + '\n' +
-          Math.round(d.data.percentage) + '%');
+        $('#slice_image').attr('src', chartComponent.slicesImages[d.data.event]);
+        $('#slice_value').text('label: ' + d.data.label + ':'  + '\n' +
+          'value: ' + d.data.value + '\n' +
+          'percentage: ' + Math.round(d.data.percentage) + '%');
       } else {
         d3.select('#centerData').style('display', 'none');
       }
-    });
-
-    $('#pieChart').ready(() => {
-      $('#center_image').bind('click', (event) => {
-        alert('1');
-      });
     });
   }
 }
